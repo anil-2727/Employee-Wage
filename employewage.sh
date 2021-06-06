5695ec0159112c49664203d2c228b6eb669bb8d4
anil-2727 <bali39959@gmail.com>
2021-06-06 13:07:20 +0530

    Added: work_hour

--- employewage.sh
+++ employewage.sh
@@ -13,35 +13,29 @@ esac
 fulldayhour=8
 parttimehour=4
 wagephour=20
-month=1
+month=0
 totalhr=0
 totalwage=0
 
-while [[ $month != 21 ]];
-do
+function workhours
+{
+while [[ $month != 21 ]];do
+
 W=$((4+((4*$((RANDOM%2))))))
 case "$W" in
 "8")
-echo wage for fullday: "$(($fulldayhour*$wagephour))"
 totalhr=$(($totalhr+$W))
 ((month++))
 ;;
 
 "4")
-echo part time wage  : "$(($parttimehour*$wagephour))"
 totalhr=$(($totalhr+$W))
 ((month++))
 ;;
 esac
-if [[ $totalhr -ge 100 ]];then
-echo 100 hours limit reached
-break
-fi
-
 done
+echo total work hours are "$totalhr"
+}
 
-totalwage=$(($totalhr*20))
-echo wage for working for a month:"$totalwage"
-
-
+workhours
 
