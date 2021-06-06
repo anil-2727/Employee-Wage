a38e474fb801db2c30a63b98bd1444ef05aad7a8
anil-2727 <bali39959@gmail.com>
2021-06-06 13:11:14 +0530

    Added: store_daily_wage

--- employewage.sh
+++ employewage.sh
@@ -1,5 +1,7 @@
 #  Welcome to Employee Wage Computation Program on Master Branch
 
+declare -A dandtwage
+
 x=$((RANDOM%2))
 case "$x" in
 "1")
@@ -16,6 +18,8 @@ wagephour=20
 month=0
 totalhr=0
 totalwage=0
+d=0
+dailywage=0
 
 function workhours
 {
@@ -24,17 +28,29 @@ while [[ $month != 21 ]];do
 W=$((4+((4*$((RANDOM%2))))))
 case "$W" in
 "8")
+dailywage=$(($W*20))
 totalhr=$(($totalhr+$W))
+	dandtwage[$d]=$dailywage
+echo dailywage :${dandtwage[$d]}
+((d++))
 ((month++))
 ;;
 
 "4")
+dailywage=$(($W*20))
 totalhr=$(($totalhr+$W))
+dandtwage[$d]=$dailywage
+echo dailywage :${dandtwage[$d]}
+((d++))
 ((month++))
 ;;
 esac
 done
+
 echo total work hours are "$totalhr"
+totalwage=$(($totalhr*20))
+dandtwage[$d]=$totalwage
+echo total wage : ${dandtwage[$d]}
 }
 
 workhours
