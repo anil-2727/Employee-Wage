47130d064da15bccb01f2291caf3ec85db7bb699
anil-2727 <bali39959@gmail.com>
2021-06-06 12:02:18 +0530

    Added: present or absent

---
+++ employewage.sh
@@ -0,0 +1,10 @@
+# Welcome to Employee Wage Computation Program on Master Branch
+
+ispresent=1
+x=$((RANDOM%2))
+if [ $x -eq 1 ]
+then
+echo " employe is present"
+else 
+echo "employe is absent"
+fi
